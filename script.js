279ecd44de78d1fb02c34d9c2a3d1c36c2a9b555
cauanrricardo <cauanrricardo@gmail.com>
2025-10-01 23:56:43 -0300

    Versao 1.0 do sistema de login'

---
+++ script.js
@@ -0,0 +1,17 @@
+function login(){
+    const usuario = document.getElementById("usuario").value;
+    const senha = document.getElementById("senha").value;
+    const msg = document.getElementById("msg");
+
+ if ((usuario === "estagiario01" && senha === "Sm4ll") ||
+    (usuario === "admin" && senha === "bigboss@")) {
+    msg.style.color = "green";
+    msg.innerText = "Login realizado com sucesso!";
+} else {
+    msg.style.color = "red";
+    msg.innerText = "Acesso negado!";
+}
+
+}
+
+
